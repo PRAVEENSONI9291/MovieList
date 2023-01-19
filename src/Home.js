f77631c3f10f3c02cb50b2cda4dd87d36db1f39f
Praveen Soni <praveensoni9291@gmail.com>
2023-01-20 05:11:16 +0530

    seventh commit

--- src/Home.js
+++ src/Home.js
@@ -7,14 +7,18 @@ import Moviecard from './Moviecard';
 const Home = () => {
   return (
     <>
-    <div className='headline'>
+    <div className='headline container-fluid'>
     <h1>Movie List</h1>
     <div className='card-grid'>
         <Moviecard/>
         
     </div>
     
+    
    
+
+    </div>
+    <div>
 
     </div>
       
--- src/Moviecard.js
+++ src/Moviecard.js
@@ -2,31 +2,34 @@ import React from "react";
 import { movies } from "./MovieData";
 
 const Moviecard = () => {
-  
-
   return (
     <>
       {movies?.map((i, j) => {
         return (
-          <div className="card" style={{ width: "18rem" }} key={j}>
-            <img
-              src={require(`./` + i.img.src)}
-              className="card-img-top"
-              alt={i.img.alt}
-            />
-            
-              <div className="title">
-                <h5 className="card-title">
-                  {i.title + " " + `(` + i.year + `)`}
-                </h5>
-              </div>
-              <div className="distributor">
-                <p className="card-text">{i.distributor}</p>
-              </div>
-              <div className="collection"></div>
-              <p className="card-text">{i.amount}</p>
+          <div
+            className="card position-relative"
+            style={{ width: "18rem" }}
+            key={j}
+          >
+            <div>
+              <img
+                src={require(`./` + i.img.src)}
+                className="card-img-top img-fluid rounded-3"
+                alt={i.img.alt}
+              />
             </div>
-          
+
+            <div className="title">
+              <h5 className="card-title">
+                {i.title + " " + `(` + i.year + `)`}
+              </h5>
+            </div>
+            <div className="distributor">
+              <p className="card-text">{i.distributor}</p>
+            </div>
+            <div className="collection"></div>
+            <p className="card-text">Collection : {i.amount}</p>
+          </div>
         );
       })}
     </>
